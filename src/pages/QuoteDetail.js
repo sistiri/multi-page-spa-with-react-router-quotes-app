2a48f47f8934f2406b4488fb19b1ed1781983fc4
sistiri <sistiri@yahoo.com>
2022-02-15 09:57:58 +0100

    Adding Nested Route

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,12 +1,21 @@
 import { Fragment } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Route } from "react-router-dom";
+
+import Comments from '../components/comments/Comments'
+
 
 const QuoteDetail = () => {
   const params = useParams();
+  
   return (
     <Fragment>
       <h1>Quote Detail Page</h1>
       <p>{params.quoteId}</p>
+      <Route path={`/quotes/${params.quoteId}/comments`}>
+      <Comments />
+      </Route>
+      {/* <Route path={`/quotes/:quoteId/comments`}></Route>  - Should also work as it is not a link*/}
+      
     </Fragment>
   );
 };
