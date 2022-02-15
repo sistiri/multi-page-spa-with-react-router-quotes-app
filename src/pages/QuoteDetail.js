f9ae21bf4eb2fab06ec27ec7c2f44b2e329e242f
sistiri <sistiri@yahoo.com>
2022-02-15 13:53:43 +0100

    More Flexible Routing Code w useRouteMatch() + Location History Object

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,6 +1,5 @@
 import { Fragment } from "react";
-import { Link } from "react-router-dom";
-import { useParams, Route } from "react-router-dom";
+import { useParams, Route, Link , useRouteMatch } from "react-router-dom";
 
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
@@ -10,6 +9,8 @@ const DUMMY_QUOTES = [
   { id: "q2", author: "Sam", text: "Learning Angular is Great" },
 ];
 const QuoteDetail = () => {
+    const match = useRouteMatch();
+    console.log(match)
   const params = useParams();
 
   const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
@@ -21,15 +22,14 @@ const QuoteDetail = () => {
   return (
     <Fragment>
       <HighlightedQuote text={quote.text} author={quote.author} />
-      <p>{params.quoteId}</p>
-      <Route path={`/quotes/${params.quoteId}`} exact>
+      <Route path={match.path} exact>
         <div className="centered">
-          <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>
+          <Link className="btn--flat" to={`${match.url}/comments`}>
             Load Comments
           </Link>
         </div>
       </Route>
-      <Route path={`/quotes/${params.quoteId}/comments`}>
+      <Route path={`${match.path}/comments`}>
         <Comments />
       </Route>
       {/* <Route path={`/quotes/:quoteId/comments`}></Route>  - Should also work as it is not a link*/}
