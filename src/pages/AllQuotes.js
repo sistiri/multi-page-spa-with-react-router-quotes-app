3e76977746fe28d6d21cd9f1afc522093fbcb5d4
sistiri <sistiri@yahoo.com>
2022-02-15 10:27:15 +0100

    Adding Dummy Data & QuoteForm to NewQuote

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,5 +1,12 @@
+import QuoteList from "../components/quotes/QuoteList"
+
+const DUMMY_QUOTES = [
+     { id: 'q1', author: 'Max', text: 'Learning React is Fun' },
+    { id: 'q2', author: 'Sam', text: 'Learning Angular is Great' },
+]
+
 const AllQuotes = () => {
-    return <h1>All Quotes Page</h1>
+    return <QuoteList quotes={DUMMY_QUOTES} />
 
 }
 
