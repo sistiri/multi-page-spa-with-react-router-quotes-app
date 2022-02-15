d1085f83e1bc4bbac1712e21ec37e3cfa38ebd32
sistiri <sistiri@yahoo.com>
2022-02-15 12:35:47 +0100

    Conditionally Render Different Content Based on the URL w Nested Routes

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,31 +1,38 @@
 import { Fragment } from "react";
+import { Link } from "react-router-dom";
 import { useParams, Route } from "react-router-dom";
 
-import Comments from '../components/comments/Comments'
-import HighlightedQuote from '../components/quotes/HighlightedQuote'
+import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
 
 const DUMMY_QUOTES = [
-    { id: 'q1', author: 'Max', text: 'Learning React is Fun' },
-   { id: 'q2', author: 'Sam', text: 'Learning Angular is Great' },
-]
+  { id: "q1", author: "Max", text: "Learning React is Fun" },
+  { id: "q2", author: "Sam", text: "Learning Angular is Great" },
+];
 const QuoteDetail = () => {
   const params = useParams();
 
-  const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId)
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
 
-  if(!quote) {
-      return <p>No quote found!</p>
+  if (!quote) {
+    return <p>No quote found!</p>;
   }
-  
+
   return (
     <Fragment>
       <HighlightedQuote text={quote.text} author={quote.author} />
       <p>{params.quoteId}</p>
+      <Route path={`/quotes/${params.quoteId}`} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
       <Route path={`/quotes/${params.quoteId}/comments`}>
-      <Comments />
+        <Comments />
       </Route>
       {/* <Route path={`/quotes/:quoteId/comments`}></Route>  - Should also work as it is not a link*/}
-      
     </Fragment>
   );
 };
